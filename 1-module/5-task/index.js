a67f20df357668091ef70342b0ab97e2ddea6aab
g.kuznetsov <g.kuznetsov@bi.zone>
2022-01-30 15:11:21 +0300

    Task 5 function truncate

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,11 @@
 function truncate(str, maxlength) {
   // ваш код...
+  const max = maxlength;
+  const lenStr = str.length;
+  if (lenStr > max) {
+    const part1 = str.substr(0, max - 1) + "…";
+    return part1;
+  } else {
+    return str;
+  }
 }
