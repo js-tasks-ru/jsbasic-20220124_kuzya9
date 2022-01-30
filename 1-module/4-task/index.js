9b7268d62ef07eec209224a8895d3dcec5fd1304
g.kuznetsov <g.kuznetsov@bi.zone>
2022-01-30 14:45:23 +0300

    function check spam

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
 function checkSpam(str) {
   // ваш код...
+  const newStr = str.toLowerCase();
+  if (newStr.includes('1xBet'.toLowerCase()) || newStr.includes('XXX'.toLowerCase())) {
+    return true;
+  } else {
+    return false;
+  }
 }
