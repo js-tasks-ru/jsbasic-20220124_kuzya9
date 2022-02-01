f73ab5960065aa675ce00f297e56c754475d77c5
g.kuznetsov <g.kuznetsov@bi.zone>
2022-02-01 23:10:25 +0300

    Module 2 Task 3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,15 @@
 let calculator = {
   // ваш код
+  read(a, b) {
+    this.key1 = a;
+    this.key2 = b;
+  },
+  sum() {
+    return this.key1 + this.key2;
+  },
+  mul() {
+    return this.key1 * this.key2;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
