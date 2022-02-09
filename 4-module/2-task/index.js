e7f86cac9e47b23ce5cbf9873fdbb76c5d7ee9ca
g.kuznetsov <g.kuznetsov@bi.zone>
2022-02-09 14:43:32 +0300

    Module 4 Tasks 1-3

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
   // ваш код...
+  const countRows = table.rows.length;
+  for (let i = 0; i < countRows; i++) {
+    table.rows[i].cells[i].style.background = 'red';
+  }
 }
